e9012eda0611236d78215345223c1d981982c92b
timurbas <timurbas1@hotmail.com>
2022-07-13 12:16:08 +0200

    purchasing process done

--- components/purchasing-process/PurchasingProcess.tsx
+++ components/purchasing-process/PurchasingProcess.tsx
@@ -1,20 +1,27 @@
 import { HiCursorClick } from "react-icons/hi";
-import {
-  BsBasket2Fill,
-  BsFillCreditCardFill,
-  BsCircleFill,
-} from "react-icons/bs";
+import { BsBasket2Fill, BsFillCreditCardFill } from "react-icons/bs";
 import { FiPackage } from "react-icons/fi";
 
 const PurchasingProcess = () => {
-  const color = "rgb(34,197,94)";
   return (
-    <div className="flex flex-col items-center mt-10">
+    <div className="flex flex-col justify-center items-center w-full ">
       <h1 className="font-bold text-4xl">Købs Processen</h1>
-      <div className="flex flex-col items-center bg-orange-900 relative max-w-3xl before:w-2 before:bg-gradient-to-b from-green-500 to-green-700 before:min-h-[250px] before:absolute">
-        <div className="inset-0 flex flex-col justify-center items-center mt-5">
-          <BsCircleFill className="z-10 " />
-          <div className="h-1 w-28 bg-gradient-to-r to-green-500  via-purple-500 from-transparent z-10 absolute mr-32"></div>
+      <div className="grid grid-cols-4 gap-4 mt-10">
+        <div className="flex flex-col items-center">
+          <HiCursorClick className="w-8 h-8 text-green-700" />
+          <p className="text-lg font-semibold">Vælg dine varer</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <BsBasket2Fill className="w-8 h-8 text-green-700" />
+          <p className="text-lg font-semibold">Gå til kassen</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <BsFillCreditCardFill className="w-8 h-8 text-green-700" />
+          <p className="text-lg font-semibold">Betal for varerne</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <FiPackage className="w-8 h-8 text-green-700" />
+          <p className="text-lg font-semibold">Afhent dine varer i butikken</p>
         </div>
       </div>
     </div>
@@ -22,42 +29,3 @@ const PurchasingProcess = () => {
 };
 
 export default PurchasingProcess;
-
-<div className="flex flex-col justify-center items-center w-full ">
-  <h1>Købs Processen</h1>
-  <div className="grid grid-cols-11 gap-4 mt-10">
-    <div>
-      <p>hej1</p>
-    </div>
-    <div className="flex flex-col">
-      <p className="">Vælg dine varer</p>
-    </div>
-    <div>
-      <p>Rarrow</p>
-    </div>
-    <div>
-      <p>hej2</p>
-    </div>
-    <div className="flex flex-col">
-      <p className="">Gå til kassen</p>
-    </div>
-    <div>
-      <p>Rarrow</p>
-    </div>
-    <div>
-      <p>hej3</p>
-    </div>
-    <div className="flex flex-col">
-      <p className="">Betal</p>
-    </div>
-    <div>
-      <p>Rarrow</p>
-    </div>
-    <div>
-      <p>hej4</p>
-    </div>
-    <div className="flex flex-col">
-      <p className="">Afhent dine varer i butikken</p>
-    </div>
-  </div>
-</div>;
