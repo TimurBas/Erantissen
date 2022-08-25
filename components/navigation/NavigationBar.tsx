7c8f6a96a5170899ce80221aef28a8403b879fde
TimurBas <timurbas1@hotmail.com>
2022-08-25 12:43:15 +0200

    color

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -6,8 +6,8 @@ import ColorSwitch from "./ColorSwitch";
 
 const NavigationBar = () => {
   return (
-    <div className="sticky top-0 z-10 bg-primary dark:bg-primary-dark">
-      <div className="flex justify-center items-center ml-5">
+    <div className="sticky top-0 z-10 bg-nav dark:bg-nav-dark">
+      <div className="flex items-center justify-center ml-5">
         <div className="flex space-x-9">
           <div className="flex items-center space-x-9">
             <Home />
@@ -15,7 +15,7 @@ const NavigationBar = () => {
           </div>
           <SearchBar />
         </div>
-        <div className="flex w-full justify-end items-center gap-3 mr-5">
+        <div className="flex items-center justify-end w-full gap-3 mr-5">
           <Basket />
           <ColorSwitch />
         </div>
