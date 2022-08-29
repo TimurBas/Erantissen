03ca5b733680a1564c66aecfaa5c16e0911f7e2c
timurbas <timurbas1@hotmail.com>
2022-08-29 16:51:28 +0200

    hero section animated

--- components/hero/Carousel.tsx
+++ components/hero/Carousel.tsx
@@ -4,6 +4,7 @@ import CarouselInfo from "./CarouselInfo";
 import { HeroModel } from "../../shared/responses/HeroResponse";
 
 const Carousel = ({ heros }: { heros: HeroModel[] }) => {
+  const [fade, setFade] = useState(false);
   const [current, setCurrent] = useState(0);
   const imageUrls = heros.map((hero) => hero["imageUrl"]);
   const imageUrlsLength = imageUrls.length;
@@ -14,10 +15,14 @@ const Carousel = ({ heros }: { heros: HeroModel[] }) => {
     } else {
       current == imageUrlsLength - 1 ? setCurrent(0) : setCurrent(current + 1);
     }
+    setFade(true);
   };
 
   return (
-    <div className={`flex flex-col transition-all`}>
+    <div
+      className={`flex flex-col ${fade ? "animate-fade" : ""}`}
+      onAnimationEnd={() => setFade(false)}
+    >
       <div className="relative flex items-center">
         <LeftArrow handleClick={handleClick} />
         {imageUrlsLength == 0 ? (
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,19 @@ module.exports = {
       colors: {
         primary: "#f7fafc",
         "primary-dark": "#11342e",
-        "nav": "#F5F6F6",
+        nav: "#F5F6F6",
         "nav-dark": "#0F2E29",
-        "footer": "#F5F6F6",
-        "footer-dark": "#0F2E29"
+        footer: "#F5F6F6",
+        "footer-dark": "#0F2E29",
+      },
+      keyframes: {
+        fade: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fade: "fade 250ms ease-out both",
       },
     },
   },
