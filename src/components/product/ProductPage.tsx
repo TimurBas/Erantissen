5023392d8118edfb5125a58d0b397e97483e38a2
TimurBas <timurbas1@hotmail.com>
2022-12-11 17:27:16 +0100

    toast in product page now

--- src/components/product/ProductPage.tsx
+++ src/components/product/ProductPage.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductModel } from "../../shared/responses/ProductResponse";
+import SuccessToaster from "../toasters/SuccessToaster";
 import Product from "./Product";
 
 const ProductPage = ({ product }: { product: ProductModel }) => {
-  return <Product info={product} />;
+  const [toastTitles, setToastTitles] = useState<string[]>([]);
+
+  return (
+    <>
+      <Product info={product} setToastTitles={setToastTitles} />
+      <div className="fixed bottom-4 right-10 z-10 w-72">
+        {toastTitles.map((toastTitle) => (
+          <SuccessToaster key={toastTitle} title={toastTitle} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default ProductPage;
